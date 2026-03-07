53f17cec8e17410013c204808e2d08ceed5cf9a6
Tim Kye <tim@kye.dev>
2026-03-07 12:03:09 -0800

    update to cloudflare

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,5 +1,5 @@
 module "domain" {
-  source      = "github.com/kyeotic/tf-deno-domain-aws"
+  source      = "github.com/kyeotic/tf-deno-domain-cloudflare"
   zone_name   = var.zone_name
   domain_name = var.domain_name
   deno_acme   = var.deno_deploy_acme
